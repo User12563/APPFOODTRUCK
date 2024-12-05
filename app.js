331d31a198059445eb99c313b89f58ad0f99a32c
User12563 <95092761+User12563@users.noreply.github.com>
2024-12-05 20:41:11 +0100

    app.js

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 
+new Vue({
+    el: '#app',
+    data: {
+        categories: [
+            { name: 'Burgers' },
+            { name: 'Salades' },
+            { name: 'Desserts' },
+            { name: 'Boissons' }
+        ]
+    },
+    methods: {
+        goToMeals(category) {
+            window.location.href = 'meals.html?category=' + category;
+        }
+    }
+});
